a79f0e508f0b8a5a907495a4a6e155feb384bad9
moxing <2765227451@qq.com>
2026-04-12 13:35:59 +0800

    Delete backend/wisdom-pm directory

--- backend/wisdom-pm/wisdom-server/src/main/java/com/wisdom/mapper/HouseMapper.java
+++
@@ -1,8 +0,0 @@
-package com.wisdom.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wisdom.entity.House;
-
-public interface HouseMapper extends BaseMapper<House> {
-
-}
